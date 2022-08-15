855fc711fd90603ccfa13b5b56f40b5d44cd5a3d
xiaoshu <wx_b6b1d1a0115f49d29a763686b54c0bbe@git.code.tencent.com>
2022-08-15 23:18:58 +0800

    fix: widget拆分

--- lib/app/modules/shopping_cart/shopping_cart_page.dart
+++ lib/app/modules/shopping_cart/shopping_cart_page.dart
@@ -1,9 +1,7 @@
-import 'package:cached_network_image/cached_network_image.dart';
+import 'package:code_zero/app/modules/shopping_cart/widget/shopping_cart_goods_item.dart';
 import 'package:code_zero/common/colors.dart';
 import 'package:code_zero/generated/assets/flutter_assets.dart';
-import 'package:code_zero/utils/device_util.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import 'shopping_cart_controller.dart';
 import 'package:code_zero/common/components/status_page/status_page.dart';
 import 'package:flutter/material.dart';
@@ -60,14 +58,14 @@ class ShoppingCartPage extends GetView<ShoppingCartController> {
   Widget _rightManage() {
     return GestureDetector(
       child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 10),
+        padding: EdgeInsets.symmetric(horizontal: 10.w),
         alignment: Alignment.center,
         color: Colors.transparent,
         child: Text(
           '管理',
           style: TextStyle(
             color: Colors.white,
-            fontSize: 14,
+            fontSize: 14.sp,
           ),
         ),
       ),
@@ -78,139 +76,20 @@ class ShoppingCartPage extends GetView<ShoppingCartController> {
     return SliverToBoxAdapter(
       child: Container(
         width: 375.w,
-        height: 812.w -
-            48.w -
-            60.w -
-            64.w -
-            MediaQuery.of(context).padding.bottom -
-            MediaQuery.of(context).padding.top,
+        height:
+            812.w - 64.w - 60.w - 64.w - MediaQuery.of(context).padding.bottom,
         padding: EdgeInsets.symmetric(horizontal: 15.w),
         child: ListView.separated(
           padding: EdgeInsets.symmetric(vertical: 15.w),
           itemBuilder: (BuildContext context, int index) {
-            return _orderItem();
+            return ShoppingCartGoodsItem();
           },
           separatorBuilder: (BuildContext context, int index) {
-            return SizedBox(height: 10);
+            return SizedBox(height: 10.w);
           },
           itemCount: 13,
         ),
       ),
-    );
-  }
-
-  Widget _orderItem() {
-    return Container(
-      height: 120,
-      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Row(
-        children: [
-          Icon(
-            Icons.check_box_rounded,
-            color: Colors.green,
-          ),
-          SizedBox(width: 8),
-          ClipRRect(
-            borderRadius: BorderRadius.circular(8),
-            child: CachedNetworkImage(
-              imageUrl:
-                  'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2Ff70181a20931f7807418b722b4accf9cbd89d0c6c08a-s3JzNf_fw658&refer=http%3A%2F%2Fhbimg.b0.upaiyun.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1663056542&t=17f6675c5cb02a4c4c23dd11959387e9',
-              width: 90,
-              height: 90,
-            ),
-          ),
-          SizedBox(width: 10),
-          Expanded(
-            child: Column(
-              children: [
-                Expanded(
-                  child: Text(
-                    '1019翡翠玻璃种镶玫瑰金叶子吊坠',
-                    style: TextStyle(
-                      color: Color(0xff141519),
-                      fontSize: 15,
-                    ),
-                  ),
-                ),
-                _priceAndCount(),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _priceAndCount() {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: [
-        Text(
-          '￥',
-          style: TextStyle(
-            color: Color(0xff1BDB8A),
-            fontSize: 10,
-          ),
-        ),
-        Text(
-          '30000',
-          style: TextStyle(
-            color: Color(0xff1BDB8A),
-            fontSize: 18,
-          ),
-        ),
-        Expanded(child: SizedBox()),
-        GestureDetector(
-          child: Container(
-            alignment: Alignment.center,
-            width: 25,
-            height: 22,
-            color: Colors.transparent,
-            child: Text(
-              '-',
-              style: TextStyle(
-                color: Color(0xffD9D9D9),
-                fontSize: 18,
-              ),
-            ),
-          ),
-        ),
-        Container(
-          padding: EdgeInsets.symmetric(horizontal: 14),
-          alignment: Alignment.center,
-          height: 22,
-          decoration: BoxDecoration(
-            color: Color(0xffF5F5F5),
-            borderRadius: BorderRadius.circular(4),
-          ),
-          child: Text(
-            '1',
-            style: TextStyle(
-              color: Color(0xff111111),
-              fontSize: 15,
-            ),
-          ),
-        ),
-        GestureDetector(
-          child: Container(
-            color: Colors.transparent,
-            alignment: Alignment.center,
-            width: 25,
-            height: 22,
-            child: Text(
-              '+',
-              style: TextStyle(
-                color: Color(0xff111111),
-                fontSize: 18,
-              ),
-            ),
-          ),
-        ),
-      ],
     );
   }
 
@@ -234,7 +113,7 @@ class ShoppingCartPage extends GetView<ShoppingCartController> {
               '全选',
               style: TextStyle(
                 color: Color(0xff757575),
-                fontSize: 15,
+                fontSize: 15.sp,
               ),
             ),
             Expanded(child: SizedBox()),
@@ -242,37 +121,37 @@ class ShoppingCartPage extends GetView<ShoppingCartController> {
               '总计:',
               style: TextStyle(
                 color: Color(0xff757575),
-                fontSize: 15,
+                fontSize: 15.sp,
               ),
             ),
             Text(
               '￥',
               style: TextStyle(
                 color: Color(0xff1BDB8A),
-                fontSize: 15,
+                fontSize: 15.sp,
               ),
             ),
             Text(
               '30000',
               style: TextStyle(
                 color: Color(0xff1BDB8A),
-                fontSize: 18,
+                fontSize: 18.sp,
               ),
             ),
             SizedBox(width: 10.w),
             Container(
-              width: 100,
-              height: 40,
+              width: 100.w,
+              height: 40.w,
               alignment: Alignment.center,
               decoration: BoxDecoration(
                 color: Color(0xff1BDB8A),
-                borderRadius: BorderRadius.circular(20),
+                borderRadius: BorderRadius.circular(20.w),
               ),
               child: Text(
                 '结算',
                 style: TextStyle(
                   color: Color(0xffffffff),
-                  fontSize: 15,
+                  fontSize: 15.sp,
                 ),
               ),
             ),
---
+++ lib/app/modules/shopping_cart/widget/shopping_cart_goods_item.dart
@@ -0,0 +1,127 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:code_zero/generated/assets/flutter_assets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ShoppingCartGoodsItem extends StatelessWidget {
+  const ShoppingCartGoodsItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 120.w,
+      padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 15.w),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10.w),
+      ),
+      child: Row(
+        children: [
+          Image.asset(
+            Assets.imagesShoppingCartGoodsUnselected,
+            width: 19.w,
+            height: 19.w,
+          ),
+          SizedBox(width: 8.w),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: CachedNetworkImage(
+              imageUrl:
+                  'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2Ff70181a20931f7807418b722b4accf9cbd89d0c6c08a-s3JzNf_fw658&refer=http%3A%2F%2Fhbimg.b0.upaiyun.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1663056542&t=17f6675c5cb02a4c4c23dd11959387e9',
+              width: 90.w,
+              height: 90.w,
+            ),
+          ),
+          SizedBox(width: 10.w),
+          Expanded(
+            child: Column(
+              children: [
+                Expanded(
+                  child: Text(
+                    '1019翡翠玻璃种镶玫瑰金叶子吊坠',
+                    style: TextStyle(
+                      color: Color(0xff141519),
+                      fontSize: 15.sp,
+                    ),
+                  ),
+                ),
+                _priceAndCount(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _priceAndCount() {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        Text(
+          '￥',
+          style: TextStyle(
+            color: Color(0xff1BDB8A),
+            fontSize: 10.sp,
+          ),
+        ),
+        Text(
+          '30000',
+          style: TextStyle(
+            color: Color(0xff1BDB8A),
+            fontSize: 18.sp,
+          ),
+        ),
+        Expanded(child: SizedBox()),
+        GestureDetector(
+          onTap: () {},
+          child: Container(
+            alignment: Alignment.center,
+            width: 25.w,
+            height: 22.w,
+            color: Colors.transparent,
+            child: Text(
+              '-',
+              style: TextStyle(
+                color: Color(0xffD9D9D9),
+                fontSize: 18.sp,
+              ),
+            ),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 14.w),
+          alignment: Alignment.center,
+          height: 22.w,
+          decoration: BoxDecoration(
+            color: Color(0xffF5F5F5),
+            borderRadius: BorderRadius.circular(4),
+          ),
+          child: Text(
+            '1',
+            style: TextStyle(
+              color: Color(0xff111111),
+              fontSize: 15.sp,
+            ),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Container(
+            color: Colors.transparent,
+            alignment: Alignment.center,
+            width: 25.w,
+            height: 22.w,
+            child: Text(
+              '+',
+              style: TextStyle(
+                color: Color(0xff111111),
+                fontSize: 18.sp,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
