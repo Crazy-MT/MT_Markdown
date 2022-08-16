25abc4441d9ae10b214c2fb9fae7660ba53e6a26
lp0int <xiaoawl23@hotmail.com>
2022-08-16 17:49:33 +0800

    feat: Add settings page ui

--- lib/app/modules/mine/setting/setting_controller.dart
+++ lib/app/modules/mine/setting/setting_controller.dart
@@ -1,10 +1,14 @@
-import 'package:get/get.dart';
+import 'package:code_zero/common/colors.dart';
 import 'package:code_zero/common/components/status_page/status_page.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SettingController extends GetxController {
   final pageName = 'Setting'.obs;
   final errorMsg = "".obs;
   final pageStatus = FTStatusPageType.loading.obs;
+
+  RxList<_MenuItem> menuList = RxList<_MenuItem>();
 
   @override
   void onInit() {
@@ -14,6 +18,30 @@ class SettingController extends GetxController {
 
   initData() {
     pageStatus.value = FTStatusPageType.success;
+    initMenuList();
+  }
+
+  initMenuList() {
+    menuList.add(_MenuItem(title: "编辑资料"));
+    menuList.add(_MenuItem(title: "收货地址管理", showDivider: false));
+    menuList.add(_MenuItem(title: "修改登录密码", showTopDivider: true));
+    menuList.add(_MenuItem(title: "支付密码管理", showDivider: false));
+
+    menuList.add(_MenuItem(title: "功能反馈", showTopDivider: true));
+    menuList.add(_MenuItem(title: "C2C个人支付风险提示"));
+    menuList.add(_MenuItem(title: "委托寄售服务协议"));
+    menuList.add(_MenuItem(title: "用户需知"));
+    menuList.add(_MenuItem(title: "用户隐私政策"));
+    menuList.add(_MenuItem(title: "注销账号"));
+
+    menuList.add(_MenuItem(
+      title: "退出登录",
+      showTopDivider: true,
+      showDivider: false,
+      isCenter: true,
+      titleColor: Color(0xFFFF3939),
+      showArrow: false,
+    ));
   }
 
   @override
@@ -21,4 +49,24 @@ class SettingController extends GetxController {
   void setPageName(String newName) {
     pageName.value = newName;
   }
+}
+
+class _MenuItem {
+  final String title;
+  final bool showArrow;
+  final bool showDivider;
+  final bool showTopDivider;
+  final bool isCenter;
+  final VoidCallback? onClick;
+  final Color titleColor;
+
+  _MenuItem({
+    required this.title,
+    this.showArrow = true,
+    this.showDivider = true,
+    this.showTopDivider = false,
+    this.isCenter = false,
+    this.onClick,
+    this.titleColor = AppColors.text_dark,
+  });
 }
--- lib/app/modules/mine/setting/setting_page.dart
+++ lib/app/modules/mine/setting/setting_page.dart
@@ -1,7 +1,12 @@
-import 'setting_controller.dart';
+import 'package:code_zero/common/colors.dart';
+import 'package:code_zero/common/components/common_app_bar.dart';
 import 'package:code_zero/common/components/status_page/status_page.dart';
+import 'package:code_zero/generated/assets/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+
+import 'setting_controller.dart';
 
 class SettingPage extends GetView<SettingController> {
   const SettingPage({Key? key}) : super(key: key);
@@ -9,21 +14,146 @@ class SettingPage extends GetView<SettingController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Setting'),
+      backgroundColor: Color(0xFFF5F5F5),
+      appBar: CommonAppBar(
+        titleText: "设置",
         centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          color: Color(0xFF14181F),
+          onPressed: () {
+            Get.back();
+          },
+        ),
       ),
       body: Obx(
         () => FTStatusPage(
           type: controller.pageStatus.value,
           errorMsg: controller.errorMsg.value,
           builder: (BuildContext context) {
-            return Center(
-              child: Text("This page is :${controller.pageName}"),
+            return CustomScrollView(
+              slivers: [
+                _buildUserHeader(),
+                _buildMenuList(),
+              ],
             );
           },
         ),
       ),
+    );
+  }
+
+  _buildUserHeader() {
+    return SliverToBoxAdapter(
+      child: Column(
+        children: [
+          Container(
+            color: Colors.white,
+            width: double.infinity,
+            padding: EdgeInsets.all(20.w).copyWith(bottom: 10.w),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ClipOval(
+                  child: Image.asset(
+                    Assets.iconsMineUser,
+                    width: 60.w,
+                    height: 69.w,
+                  ),
+                ),
+                SizedBox(
+                  width: 15.w,
+                ),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      "翡翠爱好者",
+                      style: TextStyle(
+                        color: AppColors.text_dark,
+                        fontSize: 18.sp,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5.w,
+                    ),
+                    Text(
+                      "19910736696",
+                      style: TextStyle(
+                        color: Color(0xFF757575),
+                        fontSize: 14.sp,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          _buildDivider(),
+        ],
+      ),
+    );
+  }
+
+  _buildMenuList() {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (content, index) {
+          return _buildMenuItem(index);
+        },
+        childCount: controller.menuList.length,
+      ),
+    );
+  }
+
+  _buildMenuItem(index) {
+    var item = controller.menuList[index];
+    return Container(
+      color: Colors.white,
+      child: Column(
+        children: [
+          if (item.showTopDivider) _buildDivider(height: 10.w),
+          Container(
+            height: 50.w,
+            width: double.infinity,
+            margin: EdgeInsets.symmetric(horizontal: 15.w),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    item.title,
+                    textAlign: item.isCenter ? TextAlign.center : TextAlign.start,
+                    style: TextStyle(
+                      color: item.titleColor,
+                      fontSize: 14.sp,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                item.showArrow
+                    ? Icon(
+                        Icons.arrow_forward_ios,
+                        color: Color(0xFFABAAB9),
+                        size: 18.w,
+                      )
+                    : SizedBox(),
+              ],
+            ),
+          ),
+          if (item.showDivider) _buildDivider(),
+        ],
+      ),
+    );
+  }
+
+  _buildDivider({height = 1.0}) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 15.w),
+      height: height,
+      color: Color(0xFFF5F5F5),
     );
   }
 }
