1c5ce9d2ba1f730c7c400b4d9d0794f9d99f8d03
xiaoshu <wx_b6b1d1a0115f49d29a763686b54c0bbe@git.code.tencent.com>
2022-08-19 23:40:57 +0800

    fix: 加一个圆角

--- lib/app/modules/mine/collection_settings/widget/bank_card_list_widget.dart
+++ lib/app/modules/mine/collection_settings/widget/bank_card_list_widget.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class BankCardListWidget extends StatelessWidget {
   const BankCardListWidget({Key? key}) : super(key: key);
@@ -9,6 +9,14 @@ class BankCardListWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+      ),
+    );
   }
 }
--- lib/app/modules/mine/collection_settings/widget/wechat_widget.dart
+++ lib/app/modules/mine/collection_settings/widget/wechat_widget.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class WechatWidget extends StatelessWidget {
   const WechatWidget({Key? key}) : super(key: key);
@@ -9,6 +9,14 @@ class WechatWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+      ),
+    );
   }
 }
