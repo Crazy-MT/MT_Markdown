ba947332079ef4be81ed86c1b2a3611281fa2fd7
MT <ytumaotong@gmail.com>
2022-08-21 11:28:27 +0800

    feat: 去掉的两个设置

--- lib/app/modules/mine/setting/setting_controller.dart
+++ lib/app/modules/mine/setting/setting_controller.dart
@@ -30,13 +30,13 @@ class SettingController extends GetxController {
         onClick: () {
           Get.toNamed(RoutesID.USER_INFORMATION_PAGE);
         }));
-    menuList.add(_MenuItem(title: "收货地址管理", showDivider: false));
+    // menuList.add(_MenuItem(title: "收货地址管理", showDivider: false));
     menuList.add(_MenuItem(
       title: "修改登录密码",
       showTopDivider: true,
       onClick: _toResetPasswordPage,
     ));
-    menuList.add(_MenuItem(title: "支付密码管理", showDivider: false));
+    // menuList.add(_MenuItem(title: "支付密码管理", showDivider: false));
 
     menuList.add(_MenuItem(title: "功能反馈", showTopDivider: true));
     menuList.add(_MenuItem(
