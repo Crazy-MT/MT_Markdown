7e2e694274dd7f06c383de89e3380e4d6415224c
wangjin <>
2022-09-05 00:47:10 +0800

    feat: add 申诉反馈 图片选择器

--- lib/app/modules/mine/complaint_feedback/complaint_feedback_controller.dart
+++ lib/app/modules/mine/complaint_feedback/complaint_feedback_controller.dart
@@ -1,10 +1,16 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:code_zero/common/components/status_page/status_page.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
+import '../../../../common/colors.dart';
 
 class ComplaintFeedbackController extends GetxController {
   final pageName = 'ComplaintFeedback'.obs;
   final errorMsg = "".obs;
   final pageStatus = FTStatusPageType.loading.obs;
+  final textCount = 0.obs;
+  final photoItems = ["add"].obs;
 
   @override
   void onInit() {
@@ -20,5 +26,21 @@ class ComplaintFeedbackController extends GetxController {
   void onClose() {}
   void setPageName(String newName) {
     pageName.value = newName;
+  }
+
+  // 选择图片并上传
+  Future<void> addImage() async {
+    final ImagePicker _picker = ImagePicker();
+
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    if (image?.path != null) {
+      photoItems.insert(0, image!.path);
+    }
+  }
+
+  removeImage(int index) {
+    if (photoItems.length > index && photoItems[index] != "add") {
+      photoItems.removeAt(index);
+    }
   }
 }
--- lib/app/modules/mine/complaint_feedback/complaint_feedback_page.dart
+++ lib/app/modules/mine/complaint_feedback/complaint_feedback_page.dart
@@ -1,7 +1,6 @@
-import 'dart:ui';
+import 'dart:io';
 
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import '../../../../common/colors.dart';
 import '../../../../common/components/common_app_bar.dart';
 import 'complaint_feedback_controller.dart';
@@ -16,6 +15,7 @@ class ComplaintFeedbackPage extends GetView<ComplaintFeedbackController> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.bg_gray,
+      resizeToAvoidBottomInset: false,
       appBar: CommonAppBar(
         titleText: "申诉反馈",
         centerTitle: true,
@@ -32,14 +32,24 @@ class ComplaintFeedbackPage extends GetView<ComplaintFeedbackController> {
           type: controller.pageStatus.value,
           errorMsg: controller.errorMsg.value,
           builder: (BuildContext context) {
-            return Container(
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20.w),
-                      topRight: Radius.circular(20.w))),
-              margin: EdgeInsets.only(top: 10.w),
-              child: _buildContent(),
+            return Stack(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20.w),
+                          topRight: Radius.circular(20.w))),
+                  margin: EdgeInsets.only(top: 10.w),
+                  child: _buildContent(),
+                ),
+                Positioned(
+                  left: 0,
+                  right: 0,
+                  bottom: 0.w,
+                  child: _bottomBtn(context),
+                ),
+              ],
             );
           },
         ),
@@ -50,7 +60,6 @@ class ComplaintFeedbackPage extends GetView<ComplaintFeedbackController> {
   _buildContent() {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Container(
           padding:
@@ -66,48 +75,139 @@ class ComplaintFeedbackPage extends GetView<ComplaintFeedbackController> {
             color: Color(0xffF5F5F5),
             borderRadius: BorderRadius.circular(10),
           ),
-          height: 288.w,
+          // height: 288.w,
           margin: EdgeInsets.symmetric(horizontal: 20.w),
-          child: TextField(
-            style: TextStyle(
-              color: const Color(0xFF111111),
-              fontWeight: FontWeight.w400,
-              fontSize: 16.sp,
-            ),
-            maxLength: 500,
-            maxLines: 10,
-            decoration: InputDecoration(
-                border: InputBorder.none,
-                hintText: "申诉意见越全，问题才能有效解决哦～",
-                hintStyle: TextStyle(
-                    color: Color(
-                      0xffABAAB9,
-                    ),
-                    fontSize: 16.sp)),
+          child: Column(
+            children: [
+              TextField(
+                style: TextStyle(
+                  color: const Color(0xFF111111),
+                  fontWeight: FontWeight.w400,
+                  fontSize: 16.sp,
+                ),
+                onChanged: (value) {
+                  controller.textCount.value = value.length;
+                },
+                maxLength: 500,
+                maxLines: 9,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "申诉意见越全，问题才能有效解决哦～",
+                  hintStyle: TextStyle(
+                      color: Color(
+                        0xffABAAB9,
+                      ),
+                      fontSize: 16.sp),
+                ),
+              ),
+              _photoAdd(),
+            ],
           ),
         ),
-        Expanded(
-          child: SizedBox(),
-        ),
-        _bottomBtn(),
       ],
     );
   }
 
-  _bottomBtn() {
-    return Container(
-      decoration: BoxDecoration(
-        color: Color(0xff1BDB8A),
-        borderRadius: BorderRadius.circular(40.w),
-      ),
-      margin: EdgeInsets.only(left: 20.w, right: 20.w, bottom: 44.w),
-      height: 44.w,
-      child: Center(
-        child: Text(
-          "提交申诉",
-          style: TextStyle(color: Colors.white, fontSize: 16.sp),
+  _bottomBtn(BuildContext context) {
+    return Obx(
+      () => GestureDetector(
+        onTap: () {
+          if (controller.textCount.value <= 0) {
+            return;
+          }
+          print("触发提交接口---");
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            color: controller.textCount.value > 0
+                ? Color(0xff1BDB8A)
+                : Color(0xffBAEED8),
+            borderRadius: BorderRadius.circular(40.w),
+          ),
+          margin: EdgeInsets.only(
+              left: 20.w,
+              right: 20.w,
+              bottom: MediaQuery.of(context).padding.bottom + 20),
+          height: 44.w,
+          child: Center(
+            child: Text(
+              "提交申诉",
+              style: TextStyle(color: Colors.white, fontSize: 16.sp),
+            ),
+          ),
         ),
       ),
     );
+  }
+
+  _photoAdd() {
+    return Obx(
+      () => Container(
+        margin: EdgeInsets.only(top: 10.w),
+        height: 80.w,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (BuildContext context, int index) {
+            return _photoItem(index);
+          },
+          itemCount: controller.photoItems.length,
+        ),
+      ),
+    );
+  }
+
+  _photoItem(int index) {
+    return controller.photoItems[index] == "add"
+        ? GestureDetector(
+            onTap: () {
+              controller.addImage();
+            },
+            child: ClipRRect(
+              borderRadius: BorderRadius.all(
+                Radius.circular(10),
+              ),
+              child: Container(
+                height: 80.w,
+                width: 80.w,
+                color: Colors.green,
+              ),
+            ),
+          )
+        : Container(
+            margin: EdgeInsets.only(right: 10.w),
+            height: 80.w,
+            width: 80.w,
+            child: Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                  child: Obx(
+                    () => Image.file(
+                      File(controller.photoItems[index]),
+                      width: 80.w,
+                      height: 80.w,
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  right: 3.w,
+                  top: 3.w,
+                  child: GestureDetector(
+                    onTap: () {
+                      controller.removeImage(index);
+                    },
+                    child: Container(
+                      width: 20.w,
+                      height: 20.w,
+                      color: Colors.red,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
   }
 }
