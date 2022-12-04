0845b907634bca6e745d56d8a03ee3179ff60361
maotong <ytumaotong@gmail.com>
2022-12-04 13:03:58 +0800

    优化打包脚本

--- package/build.sh
+++ package/build.sh
@@ -3,6 +3,9 @@ rm -rf ./build/ios/ipa/code_zero.ipa
 rm -rf ./build/app/outputs/flutter-apk/app-release.apk
 rm -rf ./build/app/outputs/flutter-apk/app-debug.apk
 
+fvm flutter build apk --debug
+./package/pgyer_upload.sh -k 98e445172e8942aece1d0eac22f0270e ./build/app/outputs/flutter-apk/app-debug.apk
+
 fvm flutter build ipa --release --export-options-plist=./package/ExportOptions.plist
 
 #curl -F "file=@./build/ios/ipa/code_zero.ipa" -F "uKey=0b7c03ecc4729dd91d670f561f4fc46b" -F "_api_key=98e445172e8942aece1d0eac22f0270e" https://www.pgyer.com/apiv1/app/upload
@@ -10,5 +13,4 @@ fvm flutter build ipa --release --export-options-plist=./package/ExportOptions.p
 #curl -F "file=@./build/app/outputs/flutter-apk/app-debug.apk" -F "uKey=0b7c03ecc4729dd91d670f561f4fc46b" -F "_api_key=98e445172e8942aece1d0eac22f0270e" https://www.pgyer.com/apiv1/app/upload
 
 ./package/pgyer_upload.sh -k 98e445172e8942aece1d0eac22f0270e ./build/ios/ipa/code_zero.ipa
-fvm flutter build apk --debug
-./package/pgyer_upload.sh -k 98e445172e8942aece1d0eac22f0270e ./build/app/outputs/flutter-apk/app-debug.apk
+
